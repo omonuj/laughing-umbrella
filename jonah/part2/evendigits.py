9d6eedc78a575e9cfdf9ee3849ec7a194c35e3d4
jonah odoh <jonahodoh@gmail.com>
2024-07-30 12:02:41 +0100

    recent part2

---
+++ jonah/part2/evendigits.py
@@ -0,0 +1,26 @@
+#This program generates lists of even numbers from 1000 t0 3000
+
+#defines method
+even_digit_numbers = []
+
+for i in range(1000, 3001):
+    # Convert the number to a string
+    num_str = str(i)
+    # Assume all digits are even initially
+    all_even = True
+    
+    # Check each digit to see if it is even
+    for digit in num_str:
+        if int(digit) % 2 != 0:
+            all_even = False
+            break
+    
+    # If all digits are even, add the number to the list
+    if all_even:
+        even_digit_numbers.append(num_str)
+
+#uses the join method to join the list into a single string with each number separated by a comma
+result = ",".join(even_digit_numbers)
+
+#prints results
+print(result)
--- jonah/part2/sentenceanddigits.py
+++ jonah/part2/sentenceanddigits.py
@@ -1,16 +1,23 @@
+#this program takes letters and digits and prints how many letters and how many digits.
+
+#defining and setting function
 def count_letters_and_digits(sentence_digits):
 
+	#initialize variables to zero
 	letters = 0
 	digits = 0
 
+	#for statement and if statement to check if if input is alphabet or digits and store them seperately  
 	for char in sentence:
 		if char.isalpha():
 			letters += 1
 		elif char.isdigit():
 			digits += 1
 		
+#print letters and digits
 	print(f"LETTERS {letters}")
 	print(f"DIGITS {digits}")
 
+#prompt user for inputs
 sentence = input("Enter a sentence & digits: ")
 count_letters_and_digits(sentence)
